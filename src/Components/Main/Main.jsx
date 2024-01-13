e034ce04688a44ea61041cdb2ae668dff4b115fa
Metaldetect <122224196+Metaldetect@users.noreply.github.com>
2024-01-13 04:07:22 +0100

    Create Main.jsx

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { MainContainer, Title } from "./Main.styled";
+import Button from "../Button";
+import Modal from "../Modal";
+
+function Main() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <MainContainer>
+      <Title>Швидка доставка ваших почуттів</Title>
+      <Button onClick={openModal} />
+      {isModalOpen && <Modal onClose={closeModal} />}
+    </MainContainer>
+  );
+}
+
+export default Main;
