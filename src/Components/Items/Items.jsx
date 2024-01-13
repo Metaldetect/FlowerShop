39fac783108371254de009873a725d2fa37bce80
Metaldetect <122224196+Metaldetect@users.noreply.github.com>
2024-01-13 04:07:16 +0100

    Create Items.jsx

---
+++ src/Components/Items/Items.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { Element } from "react-scroll";
+import {
+  ItemsBox,
+  ItemContainer,
+  Image,
+  Title,
+  StyledPagination,
+} from "./Items.styled";
+import BouquetModal from "../BouquetModal";
+import usePagination from "../PaginationUtils";
+
+function PaginatedItems() {
+  const [items, setItems] = useState([]);
+  const [perPage] = useState(3);
+  const { currentPage, offset, handlePageClick } = usePagination(0, perPage);
+  const [selectedBouquet, setSelectedBouquet] = useState(null);
+
+  useEffect(() => {
+    fetch("/data/items.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch: ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((data) => setItems(data))
+      .catch((error) => console.error("Error loading items:", error));
+  }, []);
+
+  const handleBouquetClick = (bouquet) => {
+    setSelectedBouquet(bouquet);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedBouquet(null);
+  };
+
+  const paginatedItems = items.slice(offset, offset + perPage).map((item) => (
+    <ItemContainer key={item.id} onClick={() => handleBouquetClick(item)}>
+      <Image src={`/images/bouquets/rose-bouquet.jpeg`} alt={item.title} />
+      <p>{item.desc}</p>
+      <b>{item.price}</b>
+    </ItemContainer>
+  ));
+
+  const pageCount =
+    !isNaN(items.length) && !isNaN(perPage)
+      ? Math.ceil(items.length / perPage)
+      : 0;
+
+  return (
+    <Element name="букети">
+      <Title>Букети</Title>
+      <ItemsBox>{paginatedItems}</ItemsBox>
+      <StyledPagination
+        count={pageCount}
+        page={currentPage + 1}
+        onChange={(event, value) => handlePageClick({ selected: value - 1 })}
+      />
+      {selectedBouquet && (
+        <BouquetModal
+          items={items}
+          bouquet={selectedBouquet}
+          onClose={handleCloseModal}
+        />
+      )}
+    </Element>
+  );
+}
+
+export default PaginatedItems;
