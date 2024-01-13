2fa574b2597bc55b371e57b6ff4f09e635bc1e7f
Metaldetect <122224196+Metaldetect@users.noreply.github.com>
2024-01-13 04:09:40 +0100

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,40 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from './AuthSlice';
+import { noticesPersistReducer } from './notices/noticesSlices';
+import { friendsReducer } from './OurFriends/OurFriendsSlice';
+import {NewsReducer} from './news/newsSlice';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  blacklist: ['isFirstLoggedIn'],
+  //whitelist: ['token'],
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    notices: noticesPersistReducer,
+    news: NewsReducer,
+    friends: friendsReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
