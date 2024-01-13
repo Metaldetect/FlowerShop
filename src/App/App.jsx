978fb1eaa1663ab67a448fdf82d97396b0286c1e
Metaldetect <122224196+Metaldetect@users.noreply.github.com>
2024-01-13 04:06:48 +0100

    Create App.jsx

---
+++ src/App/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Wrapper } from "./App.styled";
+import { GlobalStyle } from "../GlobalStyles";
+import Header from "../Components/Header";
+import Main from "../Components/Main";
+import Items from "../Components/Items";
+import Footer from "../Components/Footer";
+import Delivery from "../Components/Delivery";
+import Review from "../Components/Review";
+
+const App = () => {
+  return (
+    <Router>
+      <Wrapper>
+        <GlobalStyle />
+        <Header />
+        <Main />
+        <Items />
+        <Delivery />
+        <Review />
+        <Footer />
+      </Wrapper>
+    </Router>
+  );
+};
+
+export default App;
