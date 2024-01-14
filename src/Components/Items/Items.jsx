5b59c69292ef69e56f7bd4b300593edb3a6009e4
Metaldetect <122224196+Metaldetect@users.noreply.github.com>
2024-01-14 15:54:11 +0100

    Update Items.jsx

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -19,7 +19,7 @@ function PaginatedItems() {
 
   useEffect(() => {
     axios
-      .get('/data/items.json')
+      .get(`${process.env.PUBLIC_URL}/data/items.json`)
       .then(response => {
         setItems(response.data);
       })
