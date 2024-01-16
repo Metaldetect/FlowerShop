2eb641655f3b94873535858a299297c0216db3d4
Metaldetect <122224196+Metaldetect@users.noreply.github.com>
2024-01-16 09:37:20 +0100

    Update Header.styled.js

--- src/Components/Header/Header.styled.js
+++ src/Components/Header/Header.styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
   padding: 1.5625rem;
@@ -6,15 +6,6 @@ export const HeaderContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-
-  // @media screen and (min-width: 768px) {
-  //   margin-top: 1.5rem;
-  //   height: 2.75rem;
-  // }
-  // @media screen and (min-width: 1280px) {
-  //   height: 3rem;
-  //   margin-top: 1.25rem;
-  // }
 `;
 
 export const Span = styled.span`
