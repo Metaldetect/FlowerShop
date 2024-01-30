5a8d462f1f01d37a6bca49dc3843a9ef9ab26327
Metaldetect <122224196+Metaldetect@users.noreply.github.com>
2024-01-30 14:00:11 +0100

    Update Header.styled.js

--- src/Components/Header/Header.styled.js
+++ src/Components/Header/Header.styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
+  position: relative;
   padding: 1.5625rem;
   background-color: var(--green);
   display: flex;
@@ -12,4 +13,21 @@ export const Span = styled.span`
   font-weight: 600;
   font-size: 2.5rem;
   color: var(--white);
+`;
+
+export const MenuContainer = styled.div`
+  display: flex;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const BurgerMenuContainer = styled.div`
+  @media (min-width: 769px) {
+    display: none;
+
+    @media (max-width: 768px) {
+      display: flex;
+  }
 `;
