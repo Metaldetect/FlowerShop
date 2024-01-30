f46fdd0590b2d84d9778100e5d949b19c8b81494
Metaldetect <122224196+Metaldetect@users.noreply.github.com>
2024-01-30 13:59:59 +0100

    Update App.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Wrapper } from "./App.styled";
-import { GlobalStyle } from "../GlobalStyles";
-import Header from "../Components/Header";
-import Main from "../Components/Main";
-import Items from "../Components/Items";
-import Footer from "../Components/Footer";
-import Delivery from "../Components/Delivery";
-import Review from "../Components/Review";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Wrapper } from './App.styled';
+import { GlobalStyle } from '../GlobalStyles';
+import Header from '../Components/Header';
+import Main from '../Components/Main';
+import Items from '../Components/Items';
+import Footer from '../Components/Footer';
+import Delivery from '../Components/Delivery';
+import Review from '../Components/Review';
 
 const App = () => {
   return (
