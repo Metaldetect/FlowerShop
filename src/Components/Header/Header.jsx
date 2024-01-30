41067064781a32fe16a47a10369c390675e7622b
Metaldetect <122224196+Metaldetect@users.noreply.github.com>
2024-01-30 14:00:09 +0100

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { HeaderContainer, Span } from "./Header.styled";
-import HeaderMenuList from "./Menu/Menu";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import BurgerMenu from './BurgerMenu';
+import { HeaderContainer, Span, MenuContainer } from './Header.styled';
+import HeaderMenuList from './Menu/Menu';
 
 function Header() {
-  const menuItem = ["Букети", "Доставка", "Відгуки", "Контакти"];
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuItem = ['Букети', 'Доставка', 'Відгуки', 'Контакти'];
 
   return (
     <HeaderContainer>
       <NavLink to="/">
         <Span>FlowerLover</Span>
       </NavLink>
-      <HeaderMenuList items={menuItem} />
+      <BurgerMenu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
+      <MenuContainer isOpen={isMenuOpen}>
+        <HeaderMenuList items={menuItem} />
+      </MenuContainer>
     </HeaderContainer>
   );
 }
